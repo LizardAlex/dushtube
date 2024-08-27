bca36b5ce05083d62d02e64354b33636c5d815a8
LizardAlex <alexandr-91@bk.ru>
2024-08-27 21:10:53 +0300

    u

--- serv2.py
+++ serv2.py
@@ -1,7 +1,6 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, Response, stream_with_context
 import yt_dlp
-import io
-import requests  # Импортируем requests
+import requests
 
 app = Flask(__name__)
 
@@ -14,28 +13,32 @@ def watch():
     video_url = f"https://www.youtube.com/watch?v={video_id}"
 
     try:
+        print(f"Requesting video: {video_url}")
+        
         ydl_opts = {
             'format': 'best',
             'noplaylist': True,
             'quiet': True,
         }
 
-        # Загружаем видео в буфер
-        buffer = io.BytesIO()
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             result = ydl.extract_info(video_url, download=False)
             video_stream_url = result['url']
+            print(f"Video stream URL retrieved: {video_stream_url}")
 
-        # Используем requests для получения содержимого видеопотока
-        video_content = requests.get(video_stream_url).content
+        # Получаем видеопоток в режиме стриминга
+        video_stream = requests.get(video_stream_url, stream=True)
 
-        # Отправляем содержимое клиенту
-        return send_file(io.BytesIO(video_content),
-                         as_attachment=False,
-                         mimetype='video/mp4',
-                         download_name='video.mp4')
+        def generate():
+            for chunk in video_stream.iter_content(chunk_size=1024):
+                if chunk:
+                    yield chunk
+
+        # Отправляем поток данных клиенту напрямую
+        return Response(stream_with_context(generate()), content_type='video/mp4')
 
     except Exception as e:
+        print(f"Error during streaming: {str(e)}")
         return f"Error: {str(e)}", 500
 
 if __name__ == '__main__':
